8dcc15386d32d06edd5541d267743f40c469cc41
SilviuASY <asycompany@yahoo.com>
2024-12-05 23:55:05 +0100

    evm-contract.sh

--- evm-contract.sh
+++ evm-contract.sh
@@ -52,7 +52,7 @@ input_required_details() {
         rm "$SCRIPT_DIR/token_deployment/.env"
     fi
 
-    read -s "Enter your Private Key: " PRIVATE_KEY
+    read -p "Enter your Private Key: " PRIVATE_KEY
     read -p "Enter the token name (e.g., My Token): " TOKEN_NAME
     read -p "Enter the token symbol (e.g., ASY): " TOKEN_SYMBOL
     read -p "Enter the network rpc url: " RPC_URL
